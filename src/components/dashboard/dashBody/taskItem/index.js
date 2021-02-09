52eb11bf16bfc5ac812f40e9d1977cdb096891c0
developer <tiko0000.80.tm@gmail.com>
2021-02-09 12:18:58 +0400

    update last with taskItemDelete

--- src/components/dashboard/dashBody/taskItem/index.js
+++ src/components/dashboard/dashBody/taskItem/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './index.module.css';
-
+import db from '../../../../firebase';
 
 
 const TaskItem = ({data}) =>{
+    const taskItemRef = useRef()
+
+
 
     const handlerDragStart = e => {
         setTimeout(() => {
@@ -17,8 +20,15 @@ const TaskItem = ({data}) =>{
         },0)
     }
 
+    const handleDeleteClick = e => {
+        taskItemRef.current.classList.add(styles.hideItem)
 
-
+        db.collection('tasks')
+            .doc( data.id )
+            .delete()
+            .then( () => console.log('Item is Deleted !!!') )
+            .catch( err => console.error(err.message) )
+    }
 
 
 
@@ -29,11 +39,13 @@ const TaskItem = ({data}) =>{
             draggable={true}
             onDragStart={handlerDragStart}
             onDragEnd={handlerDragEnd}
+            data-id={data.id}
+            ref={taskItemRef}
         >
             {data.data.title}
-            <div className={styles.taskDelEdit}>
+            <div className={styles.taskDelEdit} >
 
-                <svg width="16" height="16" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                <svg width="16" height="16" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" onClick={handleDeleteClick}>
                     <path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0" fill="#f44336"/>
                     <path d="m350.273438 320.105469c8.339843 8.34375 8.339843 21.824219 0 30.167969-4.160157 4.160156-9.621094 6.25-15.085938 6.25-5.460938 0-10.921875-2.089844-15.082031-6.25l-64.105469-64.109376-64.105469 64.109376c-4.160156 4.160156-9.621093 6.25-15.082031 6.25-5.464844 0-10.925781-2.089844-15.085938-6.25-8.339843-8.34375-8.339843-21.824219 0-30.167969l64.109376-64.105469-64.109376-64.105469c-8.339843-8.34375-8.339843-21.824219 0-30.167969 8.34375-8.339843 21.824219-8.339843 30.167969 0l64.105469 64.109376 64.105469-64.109376c8.34375-8.339843 21.824219-8.339843 30.167969 0 8.339843 8.34375 8.339843 21.824219 0 30.167969l-64.109376 64.105469zm0 0" fill="#fafafa"/>
                 </svg>
